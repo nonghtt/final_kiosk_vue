867e08d0c41a6bfc157bf30d0367c8e900a598f3
YSH <shyang1208@gmail.com>
2023-07-04 15:31:12 +0900

    kakao login 및 member 관련 업데이트

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,22 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <NavBar/>
   <router-view/>
 </template>
 
+<script>
+import NavBar from '@/components/outline/NavBar'
+export default {
+  name: 'App',
+  components: { NavBar },
+
+  data() {
+    return {
+
+    }
+  }
+}
+
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
